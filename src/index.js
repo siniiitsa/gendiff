cc8fe070e73139b5c46af5b15c062afec570e476
Sergey <siniiitsa@gmail.com>
2020-03-26 17:05:15 +0200

    add arguments and format type settings

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import program from 'commander';
 
+// basics
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows the difference.');
+
+// arguments
+program
+  .arguments('<firstConfig> <secondConfig>');
+
+// options
+program
+  .option('-f, --format [type]', 'output format');
 
 export default program;
