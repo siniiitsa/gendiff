258bc2daefd20c44be9b19b59942477c6c04fca4
Sergey <siniiitsa@gmail.com>
2020-03-27 15:56:27 +0200

    add diff generating action

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import program from 'commander';
+import genDiff from './gendiff.js';
 
 // basics
 program
@@ -12,5 +13,12 @@ program
 // options
 program
   .option('-f, --format [type]', 'output format');
+
+// actions
+program
+  .action((firstConfig, secondConfig) => {
+    const diff = genDiff(firstConfig, secondConfig);
+    console.log(diff);
+  });
 
 export default program;
