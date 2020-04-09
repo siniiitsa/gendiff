fc689575b6d64058f935cee0b118e93620b8b01c
Sergey <siniiitsa@gmail.com>
2020-04-09 14:30:23 +0300

    refactor gendiff module

--- src/gendiff.js
+++ src/gendiff.js
@@ -8,67 +8,105 @@ const getJsObject = (filePath) => {
   return parse(fs.readFileSync(filePath, 'utf8'));
 };
 
-const diffTypes = {
-  unchanged: 'unchanged',
-  removed: 'removed',
-  added: 'added',
+const isObject = (value) => {
+  if (Array.isArray(value) || value === null) {
+    return false;
+  }
+  return typeof value === 'object';
 };
 
 const getSign = (type) => {
-  switch (type) {
-    case diffTypes.unchanged:
-      return ' ';
-    case diffTypes.added:
-      return '+';
-    case diffTypes.removed:
-      return '-';
-    default:
-      return null;
-  }
+  const signs = {
+    unchanged: ' ',
+    changed: ' ',
+    added: '+',
+    removed: '-',
+  };
+
+  return signs[type] || null;
 };
 
-const makeDiff = (key, value, type) => ({ key, value, type });
-
-const diffToStr = (diff, indentCount = 0) => {
-  const { key, value, type } = diff;
+const wrapWithCurlyBraces = (string, indentCount = 0) => {
   const indent = ' '.repeat(indentCount);
-  const sign = getSign(type);
-
-  return `${indent}${sign} ${key}: ${value}`;
+  return `{\n${string}\n${indent}}`;
 };
 
-const toString = (diffs) => {
-  const diffStrings = diffs
-    .map((diff) => diffToStr(diff, 2))
+const objectToString = (object, indentCount) => {
+  const indent = ' '.repeat(indentCount + 4);
+
+  const pairsString = Object.entries(object)
+    .map(([key, val]) => `${indent}${key}: ${val}`)
     .join('\n');
 
-  const wrapped = `{\n${diffStrings}\n}`;
-  return wrapped;
+  return wrapWithCurlyBraces(pairsString, indentCount);
+};
+
+const stringify = (diffColl) => {
+  const iter = (diffs, indentCount) => {
+    const indent = ' '.repeat(indentCount);
+    const diffToString = ({ key, value, type, children }) => {
+      const sign = getSign(type);
+
+      if (children) {
+        const formatedValue = wrapWithCurlyBraces(iter(children, indentCount + 4), indentCount + 2);
+        return `${indent}${sign} ${key}: ${formatedValue}`;
+      }
+
+      const formatedValue = isObject(value)
+        ? objectToString(value, indentCount + 2)
+        : value;
+
+      return `${indent}${sign} ${key}: ${formatedValue}`;
+    };
+
+    const diffString = diffs
+      .map(diffToString)
+      .join('\n');
+
+    return diffString;
+  };
+
+  return wrapWithCurlyBraces(iter(diffColl, 2));
+};
+
+const makeDiff = (type, key, value, children) => {
+  const diff = { type, key, value, children: children || null };
+  return diff;
+};
+
+const makeDiffsColl = (bef, aft) => {
+  const keys = uniq([...Object.keys(bef), ...Object.keys(aft)]).sort();
+
+  const coll = keys.reduce((acc, key) => {
+    if (!has(bef, key)) {
+      return [...acc, makeDiff('added', key, aft[key])];
+    }
+
+    if (!has(aft, key)) {
+      return [...acc, makeDiff('removed', key, bef[key])];
+    }
+
+    if (isObject(bef[key]) && isObject(aft[key])) {
+      const children = makeDiffsColl(bef[key], aft[key]);
+      return [...acc, makeDiff('changed', key, null, children)];
+    }
+
+    const newDiff = isEqual(bef[key], aft[key])
+      ? makeDiff('unchanged', key, bef[key])
+      : [makeDiff('removed', key, bef[key]), makeDiff('added', key, aft[key])];
+
+    return [...acc, newDiff].flat();
+  }, []);
+
+  return coll;
 };
 
 const genDiff = (befPath, aftPath) => {
   const bef = getJsObject(befPath);
   const aft = getJsObject(aftPath);
 
-  const keys = uniq([...Object.keys(bef), ...Object.keys(aft)]);
-
-  const diffs = keys.reduce((acc, key) => {
-    if (!has(bef, key)) {
-      return [...acc, makeDiff(key, aft[key], diffTypes.added)];
-    }
-
-    if (!has(aft, key)) {
-      return [...acc, makeDiff(key, bef[key], diffTypes.removed)];
-    }
-
-    const newDiff = isEqual(bef[key], aft[key])
-      ? makeDiff(key, bef[key], diffTypes.unchanged)
-      : [makeDiff(key, aft[key], diffTypes.added), makeDiff(key, bef[key], diffTypes.removed)];
-
-    return [...acc, newDiff];
-  }, []).flat();
-
-  return toString(diffs);
+  const diffsColl = makeDiffsColl(bef, aft);
+  return stringify(diffsColl);
 };
 
 export default genDiff;
