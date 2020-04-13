f33e6542ef5e42b53d5e6fef83c4d20b106f8849
Sergey <siniiitsa@gmail.com>
2020-04-13 13:35:35 +0300

    refactor gendiff module

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,74 +2,12 @@ import { has, isEqual, uniq } from 'lodash';
 import fs from 'fs';
 import path from 'path';
 import getParser from './parsers.js';
+import getFormatter from './formatters/index.js';
+import isObject from './helpers.js';
 
 const getJsObject = (filePath) => {
   const parse = getParser(path.extname(filePath));
   return parse(fs.readFileSync(filePath, 'utf8'));
-};
-
-const isObject = (value) => {
-  if (Array.isArray(value) || value === null) {
-    return false;
-  }
-  return typeof value === 'object';
-};
-
-const getSign = (type) => {
-  const signs = {
-    unchanged: ' ',
-    changed: ' ',
-    added: '+',
-    removed: '-',
-  };
-
-  return signs[type] || null;
-};
-
-const wrapWithCurlyBraces = (string, indentCount = 0) => {
-  const indent = ' '.repeat(indentCount);
-  return `{\n${string}\n${indent}}`;
-};
-
-const objectToString = (object, indentCount) => {
-  const indent = ' '.repeat(indentCount + 4);
-
-  const pairsString = Object.entries(object)
-    .map(([key, val]) => `${indent}${key}: ${val}`)
-    .join('\n');
-
-  return wrapWithCurlyBraces(pairsString, indentCount);
-};
-
-const stringify = (diffColl) => {
-  const iter = (diffs, indentCount) => {
-    const indent = ' '.repeat(indentCount);
-    const diffToString = ({ key, value, type, children }) => {
-      const sign = getSign(type);
-
-      if (children) {
-        const formatedValue = wrapWithCurlyBraces(
-          iter(children, indentCount + 4),
-          indentCount + 2,
-        );
-        return `${indent}${sign} ${key}: ${formatedValue}`;
-      }
-
-      const formatedValue = isObject(value)
-        ? objectToString(value, indentCount + 2)
-        : value;
-
-      return `${indent}${sign} ${key}: ${formatedValue}`;
-    };
-
-    const diffString = diffs
-      .map(diffToString)
-      .join('\n');
-
-    return diffString;
-  };
-
-  return wrapWithCurlyBraces(iter(diffColl, 2));
 };
 
 const makeDiff = (type, key, value, children) => {
@@ -104,11 +42,13 @@ const makeDiffsColl = (bef, aft) => {
   return coll;
 };
 
-const genDiff = (befPath, aftPath) => {
+const genDiff = (befPath, aftPath, format) => {
   const bef = getJsObject(befPath);
   const aft = getJsObject(aftPath);
 
+  const stringify = getFormatter(format);
   const diffsColl = makeDiffsColl(bef, aft);
+
   return stringify(diffsColl);
 };
 
