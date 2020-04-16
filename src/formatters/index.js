027482298f4b2fb16beb35fb8330887bcef467aa
Sergey <siniiitsa@gmail.com>
2020-04-16 19:30:53 +0300

    add json formatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,12 @@
 import formatAsTree from './tree.js';
 import formatAsPlain from './plain.js';
+import formatAsJson from './json.js';
 
 const getFormatter = (format) => {
   const formatters = {
     tree: formatAsTree,
     plain: formatAsPlain,
+    json: formatAsJson,
   };
 
   return formatters[format] || null;
---
+++ src/formatters/json.js
@@ -0,0 +1,3 @@
+const formatAsJson = (ast) => JSON.stringify(ast, null, 2);
+
+export default formatAsJson;
