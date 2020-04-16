106879d8a86c03a617db1bcd6e9a053dd21fd651
Sergey <siniiitsa@gmail.com>
2020-04-16 19:30:11 +0300

    refactor ini parser

--- src/parsers.js
+++ src/parsers.js
@@ -1,5 +1,18 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
+import { cloneDeepWith } from 'lodash';
+
+const parseIni = (data) => {
+  const normalizeNumbers = (value) => {
+    if (typeof value === 'string') {
+      return +value || undefined;
+    }
+    return undefined;
+  };
+
+  const obj = ini.parse(data);
+  return cloneDeepWith(obj, normalizeNumbers);
+};
 
 const getParser = (extname) => {
   switch (extname) {
@@ -8,7 +21,7 @@ const getParser = (extname) => {
     case '.yml':
       return yaml.safeLoad;
     case '.ini':
-      return ini.parse;
+      return parseIni;
     default:
       return null;
   }
