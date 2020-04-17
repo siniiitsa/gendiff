81fcaef437513d2fbec59eaf6831681748d0e267
Sergey <siniiitsa@gmail.com>
2020-04-17 17:46:10 +0300

    refactor getFormatter function

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -3,13 +3,16 @@ import formatAsPlain from './plain.js';
 import formatAsJson from './json.js';
 
 const getFormatter = (format) => {
-  const formatters = {
-    tree: formatAsTree,
-    plain: formatAsPlain,
-    json: formatAsJson,
-  };
-
-  return formatters[format] || null;
+  switch (format) {
+    case 'tree':
+      return formatAsTree;
+    case 'json':
+      return formatAsJson;
+    case 'plain':
+      return formatAsPlain;
+    default:
+      throw new Error(`Unknown format option: "${format}". Use one of these instead: "tree", "json", "plain".`);
+  }
 };
 
 export default getFormatter;
