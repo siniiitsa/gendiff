4b5cf93f020a5f8c8b7370ca89293d4a0513844c
Sergey <siniiitsa@gmail.com>
2020-04-19 23:56:57 +0300

    remove unnecessary comments

--- src/index.js
+++ src/index.js
@@ -2,20 +2,16 @@ import path from 'path';
 import program from 'commander';
 import genDiff from './gendiff.js';
 
-// basics
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows the difference.');
 
-// arguments
 program
   .arguments('<firstConfig> <secondConfig>');
 
-// options
 program
   .option('-f, --format [type]', 'output format', 'tree');
 
-// actions
 program
   .action((firstConfig, secondConfig) => {
     const path1 = path.resolve(firstConfig);
