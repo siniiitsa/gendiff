fa96a5ab47845ca92fcd779324b617e41f24cde2
Sergey <siniiitsa@gmail.com>
2020-04-22 18:41:02 +0300

    fix plain formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,7 +1,7 @@
 import { isPlainObject } from 'lodash';
 
 const formatValue = (value) => {
-  if (isObject(value)) {
+  if (isPlainObject(value)) {
     return '[complex value]';
   }
 
@@ -9,64 +9,63 @@ const formatValue = (value) => {
     return `'${value}'`;
   }
 
-  return value;
+  return `${value}`;
 };
 
-const itemToString = ({ status, path, value, oldValue, newValue }) => {
-  switch (status) {
+const itemToString = (item) => {
+  switch (item.status) {
     case 'deleted':
-      return `Property '${path}' was deleted`;
+      return `Property '${item.path}' was deleted`;
     case 'added':
-      return `Property '${path}' was added with value: ${formatValue(value)}`;
-    case 'changed':
-      return `Property '${path}' was changed from ${formatValue(oldValue)} to ${formatValue(newValue)}`;
+      return `Property '${item.path}' was added with value: ${formatValue(item.value)}`;
+    case 'changed_value':
+      return `Property '${item.path}' was changed from ${formatValue(item.oldValue)} to ${formatValue(item.newValue)}`;
     default:
-      throw new Error(`Unknown ast node status: "${status}".`);
+      throw new Error(`Unknown ast node status: "${item.status}".`);
   }
-};
-
-const groupChangedItems = (acc, item, index, items) => {
-  const isFirstPairItem = (index < items.length - 1) && (items[index + 1].path === item.path);
-  if (isFirstPairItem) {
-    const combinedItem = {
-      ...item,
-      status: 'changed',
-      oldValue: item.value,
-      newValue: items[index + 1].value,
-    };
-
-    return [...acc, combinedItem];
-  }
-
-  const isSecondPairItem = (index !== 0) && (items[index - 1].path === item.path);
-  if (isSecondPairItem) {
-    return acc;
-  }
-
-  return [...acc, item];
 };
 
 const formatAsPlain = (ast) => {
   const iter = (node, pathAcc) => {
-    const { key, value, status, children } = node;
+    const {
+      key,
+      value,
+      oldValue,
+      newValue,
+      status,
+      children,
+    } = node;
+
     const currentPath = pathAcc.length > 0 ? `${pathAcc}.${key}` : key;
 
     if (status === 'unchanged') {
       return null;
     }
 
-    if (children) {
+    if (status === 'changed_children') {
       return children.map((child) => iter(child, currentPath));
     }
 
-    return { status, path: currentPath, value };
+    if (status === 'changed_value') {
+      return {
+        status,
+        path: currentPath,
+        oldValue,
+        newValue,
+      };
+    }
+
+    return {
+      status,
+      path: currentPath,
+      value,
+    };
   };
 
   const plainDiff = ast
     .map((node) => iter(node, ''))
     .flat(Infinity)
     .filter(Boolean)
-    .reduce(groupChangedItems, [])
     .map(itemToString)
     .join('\n');
 
