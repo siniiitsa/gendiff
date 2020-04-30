7370d103ed49ff35e5427c037519a0a13506ec05
Sergey <siniiitsa@gmail.com>
2020-04-30 16:33:38 +0300

    refactor plain formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -12,64 +12,28 @@ const formatValue = (value) => {
   return `${value}`;
 };
 
-const itemToString = (item) => {
-  switch (item.status) {
-    case 'deleted':
-      return `Property '${item.path}' was deleted`;
-    case 'added':
-      return `Property '${item.path}' was added with value: ${formatValue(item.value)}`;
-    case 'changed_value':
-      return `Property '${item.path}' was changed from ${formatValue(item.oldValue)} to ${formatValue(item.newValue)}`;
-    default:
-      throw new Error(`Unknown ast node status: "${item.status}".`);
-  }
-};
+const formatAsPlain = (ast, initialPath = '') => {
+  const iter = (node, ancestry) => {
+    const path = ancestry.length > 0 ? `${ancestry}.${node.key}` : node.key;
 
-const formatAsPlain = (ast) => {
-  const iter = (node, pathAcc) => {
-    const {
-      key,
-      value,
-      oldValue,
-      newValue,
-      status,
-      children,
-    } = node;
-
-    const currentPath = pathAcc.length > 0 ? `${pathAcc}.${key}` : key;
-
-    if (status === 'unchanged') {
-      return null;
+    switch (node.type) {
+      case 'deleted':
+        return `Property '${path}' was deleted`;
+      case 'added':
+        return `Property '${path}' was added with value: ${formatValue(node.value)}`;
+      case 'changed':
+        return `Property '${path}' was changed from ${formatValue(node.oldValue)} to ${formatValue(node.newValue)}`;
+      case 'nested':
+        return formatAsPlain(node.children, path);
+      default:
+        throw new Error(`Unknown ast node type: "${node.type}".`);
     }
-
-    if (status === 'changed_children') {
-      return children.map((child) => iter(child, currentPath));
-    }
-
-    if (status === 'changed_value') {
-      return {
-        status,
-        path: currentPath,
-        oldValue,
-        newValue,
-      };
-    }
-
-    return {
-      status,
-      path: currentPath,
-      value,
-    };
   };
 
-  const plainDiff = ast
-    .map((node) => iter(node, ''))
-    .flat(Infinity)
-    .filter(Boolean)
-    .map(itemToString)
+  return ast
+    .filter((node) => node.type !== 'unchanged')
+    .flatMap((node) => iter(node, initialPath))
     .join('\n');
-
-  return plainDiff;
 };
 
 export default formatAsPlain;
