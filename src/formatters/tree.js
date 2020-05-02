f966c9811138ff1419e8f9d833410a4dade3ee92
Sergey <siniiitsa@gmail.com>
2020-05-02 17:29:40 +0300

    reafactor tree formatter

--- src/formatters/tree.js
+++ src/formatters/tree.js
@@ -1,28 +1,32 @@
 import { isPlainObject } from 'lodash';
 
-const makeIndent = (indentCount, sign) => (
-  `${' '.repeat(indentCount - 2)}${sign || ' '} `
-);
+const INDENT_STEP = 4;
+
+const makeIndent = (indentCount, sign) => {
+  const emptyPart = ' '.repeat(indentCount - 2);
+  const signPart = sign ? `${sign} ` : '  ';
+  return `${emptyPart}${signPart}`;
+};
 
 const wrapCurlyBraces = (content, indentCount) => {
-  const closingBraceIndent = makeIndent(indentCount).slice(4);
+  const closingBraceIndent = makeIndent(indentCount).slice(INDENT_STEP);
   return `{\n${content}\n${closingBraceIndent}}`;
 };
 
 const formatObject = (object, indentCount) => {
-  const indent = makeIndent(indentCount + 4);
+  const indent = makeIndent(indentCount + INDENT_STEP);
   const objectContent = Object.entries(object)
     .map(([key, value]) => `${indent}${key}: ${value}`)
     .join('\n');
 
-  return wrapCurlyBraces(objectContent, indentCount + 4);
+  return wrapCurlyBraces(objectContent, indentCount + INDENT_STEP);
 };
 
 const formatValue = (value, indentCount) => (
   isPlainObject(value) ? formatObject(value, indentCount) : `${value}`
 );
 
-const formatAsTree = (ast, indentCount = 4) => {
+const formatAsTree = (ast, indentCount = INDENT_STEP) => {
   const iter = (node) => {
     switch (node.type) {
       case 'added': {
@@ -52,7 +56,7 @@ const formatAsTree = (ast, indentCount = 4) => {
       }
       case 'nested': {
         const indent = makeIndent(indentCount);
-        const children = formatAsTree(node.children, indentCount + 4);
+        const children = formatAsTree(node.children, indentCount + INDENT_STEP);
         return `${indent}${node.key}: ${children}`;
       }
       default: {
