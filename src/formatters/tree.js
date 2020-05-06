f8d2d9397b541244c1c9facef25a51175d269096
Sergey <siniiitsa@gmail.com>
2020-05-06 21:30:31 +0300

    refactor tree formatter

--- src/formatters/tree.js
+++ src/formatters/tree.js
@@ -1,62 +1,60 @@
 import { isPlainObject } from 'lodash';
 
-const INDENT_STEP = 4;
-
-const makeIndent = (indentCount, sign) => {
-  const emptyPart = ' '.repeat(indentCount - 2);
-  const signPart = sign ? `${sign} ` : '  ';
-  return `${emptyPart}${signPart}`;
+const makeIndent = (depth, sign) => {
+  const indentStep = 4;
+  const indent = ' '.repeat(depth * indentStep);
+  return sign === undefined ? indent : `${indent.slice(2)}${sign} `;
 };
 
-const wrapCurlyBraces = (content, indentCount) => {
-  const closingBraceIndent = makeIndent(indentCount).slice(INDENT_STEP);
+const wrapCurlyBraces = (content, depth) => {
+  const closingBraceIndent = makeIndent(depth - 1);
   return `{\n${content}\n${closingBraceIndent}}`;
 };
 
-const formatObject = (object, indentCount) => {
-  const indent = makeIndent(indentCount + INDENT_STEP);
+const formatObject = (object, depth) => {
+  const indent = makeIndent(depth + 1);
   const objectContent = Object.entries(object)
     .map(([key, value]) => `${indent}${key}: ${value}`)
     .join('\n');
 
-  return wrapCurlyBraces(objectContent, indentCount + INDENT_STEP);
+  return wrapCurlyBraces(objectContent, depth + 1);
 };
 
 const formatValue = (value, indentCount) => (
   isPlainObject(value) ? formatObject(value, indentCount) : `${value}`
 );
 
-const formatAsTree = (ast, indentCount = INDENT_STEP) => {
+const formatAsTree = (ast, depth = 1) => {
   const iter = (node) => {
     switch (node.type) {
       case 'added': {
-        const indent = makeIndent(indentCount, '+');
-        const value = formatValue(node.value, indentCount);
+        const indent = makeIndent(depth, '+');
+        const value = formatValue(node.value, depth);
         return `${indent}${node.key}: ${value}`;
       }
       case 'deleted': {
-        const indent = makeIndent(indentCount, '-');
-        const value = formatValue(node.value, indentCount);
+        const indent = makeIndent(depth, '-');
+        const value = formatValue(node.value, depth);
         return `${indent}${node.key}: ${value}`;
       }
       case 'unchanged': {
-        const indent = makeIndent(indentCount);
-        const value = formatValue(node.value, indentCount);
+        const indent = makeIndent(depth);
+        const value = formatValue(node.value, depth);
         return `${indent}${node.key}: ${value}`;
       }
       case 'changed': {
-        const indentAdded = makeIndent(indentCount, '+');
-        const indentDeleted = makeIndent(indentCount, '-');
-        const newValue = formatValue(node.newValue, indentCount);
-        const oldValue = formatValue(node.oldValue, indentCount);
+        const deletedIndent = makeIndent(depth, '-');
+        const addedIndent = makeIndent(depth, '+');
+        const newValue = formatValue(node.newValue, depth);
+        const oldValue = formatValue(node.oldValue, depth);
         return [
-          `${indentDeleted}${node.key}: ${oldValue}`,
-          `${indentAdded}${node.key}: ${newValue}`,
+          `${deletedIndent}${node.key}: ${oldValue}`,
+          `${addedIndent}${node.key}: ${newValue}`,
         ];
       }
       case 'nested': {
-        const indent = makeIndent(indentCount);
-        const children = formatAsTree(node.children, indentCount + INDENT_STEP);
+        const indent = makeIndent(depth);
+        const children = formatAsTree(node.children, depth + 1);
         return `${indent}${node.key}: ${children}`;
       }
       default: {
@@ -69,7 +67,7 @@ const formatAsTree = (ast, indentCount = INDENT_STEP) => {
     .flatMap((node) => iter(node))
     .join('\n');
 
-  return wrapCurlyBraces(tree, indentCount);
+  return wrapCurlyBraces(tree, depth);
 };
 
 export default formatAsTree;
